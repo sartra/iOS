6953041448c957d46563510d4aabb55f0990db2b
sartra <strenshaw@gmail.com>
2016-04-24 12:12:55 -0400

    scroll view with page control

--- day2/day2/day2/ViewController.swift
+++ day2/day2/day2/ViewController.swift
@@ -8,16 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {  // conforming to the 'delegate protocol'
     
     @IBOutlet var nameField: UITextField!
 
     var namesArray = Array<String>()
+    
+    @IBOutlet var imagesScroll: UIScrollView!
+    
+    @IBOutlet var pageContol: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstSlide = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        firstSlide.backgroundColor = UIColor.blueColor()
+        self.imagesScroll.addSubview(firstSlide)
+        
+        let secondSlide = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
+        firstSlide.backgroundColor = UIColor.greenColor()
+        self.imagesScroll.addSubview(secondSlide)
+        
+        self.imagesScroll.contentSize = CGSize(width: 400, height: 0)
+        self.imagesScroll.delegate = self
     }
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {  // control click on UIScrollViewDelegate up top
+        
+        let offset = scrollView.contentOffset.x
+        
+        if (offset > 100){
+            self.view.backgroundColor = UIColor.redColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor.lightGrayColor()
+        }
+        
+//        print("scrollViewDidScroll: \(offset)")
+    }
+
+    
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView){  // when the scrolling is ended
+        let offset = scrollView.contentOffset.x
+        print("scrollViewDidEndDecelerating: \(offset)")
+        
+        if (offset == 0){
+            print("First Page")
+            self.pageContol.currentPage = 0
+        }
+        if (offset == 200){
+            print("Second Page")
+            self.pageContol.currentPage = 1
+        }
+    }
+
     
     @IBAction func addName(){
         
@@ -29,6 +75,7 @@ class ViewController: UIViewController {
         self.nameField.text = "" //clear text field
         print("addName: \(self.namesArray)")
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

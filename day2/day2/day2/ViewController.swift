031a43ce46bb2e6f26f6ff8512f4746102aff502
sartra <strenshaw@gmail.com>
2016-04-24 11:21:20 -0400

    connected text field and button action

--- day2/day2/day2/ViewController.swift
+++ day2/day2/day2/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func addName(){
+        print("addName: ")
     }
 
     override func didReceiveMemoryWarning() {

e02f22b38f8d6db83861fa3de32fbc5fa6dfbd17
sartra <strenshaw@gmail.com>
2016-05-02 23:16:29 -0400

    added bands

---
+++ day3/movies/movies/BandsDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BandsDetailViewController.swift
+//  movies
+//
+//  Created by Sarah Renshaw on 5/1/16.
+//  Copyright © 2016 sartra. All rights reserved.
+//
+
+import UIKit
+
+class BandsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ day3/movies/movies/models/Bands.swift
@@ -0,0 +1,13 @@
+//
+//  Bands.swift
+//  movies
+//
+//  Created by Sarah Renshaw on 5/1/16.
+//  Copyright © 2016 sartra. All rights reserved.
+//
+
+import UIKit
+
+class Bands: NSObject {
+
+}
